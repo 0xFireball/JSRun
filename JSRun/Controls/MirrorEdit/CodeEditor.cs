edac25881f31e6e4471d156ec075e72a54e99c9e
0xFireball <0xFireball@outlook.com>
2016-10-19 23:34:27 -0700

    add a broken extension

---
+++ JSRun/Controls/MirrorEdit/CodeEditor.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace IridiumIon.MirrorEdit
+{
+    public class CodeEditor : TextBox
+    {
+        static CodeEditor()
+        {
+            AcceptsReturnProperty.OverrideDefaultValue(typeof(CodeEditor), true);
+            //FontFamilyProperty.OverrideDefaultValue(typeof(CodeEditor), "Consolas");
+            TextWrappingProperty.OverrideDefaultValue(typeof(CodeEditor), TextWrapping.Wrap);
+        }
+
+        public CodeEditor()
+        {
+            /*
+            AcceptsReturn = true;
+            FontFamily = "Consolas";
+            TextWrapping = TextWrapping.Wrap;
+            */
+        }
+    }
+}
